45918eba723aca6c83d63a9d1a95c4f0f7347ca8
unknown <k-sunwo@naver.com>
2023-08-17 16:35:44 +0900

    myblog: main 페이지 목록별 좋아요 버튼 추가

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+/* eslint-disable */                    // warning 안보여지게 하는 것
+
 import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
@@ -6,6 +8,19 @@ function App() {
 
   let post = '나의 코딩 일지';
   let [title, setTitle] = useState([1, 2, 3]);
+  let [like, setLike] = useState([0, 0, 0]);
+
+  function sortTitle(){
+    let copy = [...title];                   // 원본 보존(리랜더링 전까지 원본 보존)
+    copy.sort();                             // 내림차순 정렬(가나다 순)
+    setTitle(copy);
+  }
+
+  function clickLike(day) {
+    let copy = [...like];                   // 원본 보존(리랜더링 전까지 원본 보존)
+    copy[day-1] = copy[day-1] + 1;
+    setLike(copy);
+  }
 
   return (
     <div className="App">
@@ -14,9 +29,16 @@ function App() {
           { color: 'gray', fontSize: '16px' }
         }>MyBlog</h4>
       </div>
+
+      <button onClick={()=>{ sortTitle }}>가나다순 정렬</button>
+
       { title.map(day => (
         <div className='list' key={day}>
-          <h4>React Blog { day }일차</h4>
+          <h4>
+            React Blog { day }일차 
+            <span onClick={()=>{clickLike(day);}}>👍</span> 
+            { like[day-1] } 
+          </h4>
           <p>2월 17일 발행</p>
         </div>
       )) }
