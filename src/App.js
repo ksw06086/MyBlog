d35733b6329e28618e5664cc96b1dedb67cbc274
unknown <k-sunwo@naver.com>
2023-08-17 15:40:19 +0900

    myblog: main 페이지 blog 목록 구상

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '나의 코딩 일지';
+  let [title, setTitle] = useState([1, 2, 3]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='black-nav'>
+        <h4 style={
+          { color: 'gray', fontSize: '16px' }
+        }>MyBlog</h4>
+      </div>
+      { title.map(day => (
+        <div className='list' key={day}>
+          <h4>React Blog { day }일차</h4>
+          <p>2월 17일 발행</p>
+        </div>
+      )) }
     </div>
   );
 }
